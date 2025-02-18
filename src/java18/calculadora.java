bb8113bee478749eb414a47c5a84ca2f94ecd13d
natalia.silva <napereirads@gmail.com>
2025-02-18 09:19:42 -0300

    Adicionado a classe calculadora

---
+++ src/java18/calculadora.java
@@ -0,0 +1,11 @@
+package java18;
+
+public class calculadora {
+    public static int soma(int a, int b){
+        return a + b;
+    }
+    public static void main (String [] args){
+
+        System.out.println("Resultado:" + soma(5,3));
+    }
+}
