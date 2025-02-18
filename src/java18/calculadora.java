f38483d89141571685ae053a0688467f5409344e
natalia.silva <napereirads@gmail.com>
2025-02-18 09:44:01 -0300

    Adicionado a classe calculadora Adicionado subtracao

--- src/java18/calculadora.java
+++ src/java18/calculadora.java
@@ -4,8 +4,15 @@ public class calculadora {
     public static int soma(int a, int b){
         return a + b;
     }
+
+    public static int subtracao (int a, int b){
+        return a - b;
+
+    }
     public static void main (String [] args){
 
         System.out.println("Resultado:" + soma(5,3));
+        System.out.println("Resultado:" + subtracao(5, 3));
+
     }
 }
